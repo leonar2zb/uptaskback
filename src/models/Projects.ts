a3b8b46a2b482fa7ba964c0c189389e9e01dfa5d
Leo en laptop <leonar2zb@gmail.com>
2025-01-29 09:43:00 -0300

    + Creando el modelo

---
+++ src/models/Projects.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, Document } from "mongoose"
+
+// creando un tipo con las propiedades de Document y las definidas
+export type ProjectType = Document & {
+    projectName: string
+    clientName: string
+    description: string
+}
+
+// definir el esquema que coincide con el tipo anterior
+const ProjectSchema: Schema = new Schema({
+    projectName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    clientName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    }
+})
+
+// conectando ambos. Nombre único
+const Project = mongoose.model<ProjectType>('Projec', ProjectSchema)
+
+export default Project
