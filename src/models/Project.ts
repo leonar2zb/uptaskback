f82e326e0efdc5314b595b96db66763753b47332
Leo en laptop <leonar2zb@gmail.com>
2025-01-30 11:24:38 -0300

    + Relacionando modelos Project y Task

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,10 +1,12 @@
-import mongoose, { Schema, Document } from "mongoose"
+import mongoose, { Schema, Document, PopulatedDoc, Types } from "mongoose"
+import { ITask } from "./Task"
 
 // creando un tipo con las propiedades de Document y las definidas
 export interface IProject extends Document {
     projectName: string
     clientName: string
     description: string
+    tasks: PopulatedDoc<ITask>[]
 }
 
 // definir el esquema que coincide con el tipo anterior 
@@ -24,8 +26,13 @@ const ProjectSchema: Schema = new Schema({
         type: String,
         required: true,
         trim: true
+    },
+    tasks: [{
+        type: Types.ObjectId,
+        ref: 'Task'
     }
-})
+    ]
+}, { timestamps: true })
 
 // conectando ambos. Nombre único
 const Project = mongoose.model<IProject>('Projec', ProjectSchema)
