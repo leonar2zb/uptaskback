9524c17ef622864cc2991d0170eb43a145b3dfd3
Leo en laptop <leonar2zb@gmail.com>
2025-02-01 17:27:50 -0300

    * Mejorando performance con Promise.allSettled

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -9,9 +9,7 @@ export class TaskController {
             const task = new Task(req.body)
             task.project = project.id
             project.tasks.push(task) // agregar la tarea al proyecto
-            await task.save()
-            await project.save()
-            console.log(colores.bgCyan(`Creado tarea ${task.name} para ${project.projectName}`))
+            await Promise.allSettled([task.save(), project.save()])
             res.json({ "data": 'Se ha creado la tarea' })
         } catch (error) {
             console.log(colores.bgRed('Ha ocurrido un error. Detalles a continuación'))
