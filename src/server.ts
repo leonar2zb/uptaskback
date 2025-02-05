f1ed1dcd12f9723805b6d10eab06334623561d68
Leo en laptop <leonar2zb@gmail.com>
2025-02-05 18:28:21 -0300

    + Habilitado CORS

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db';
 import projectRoutes from './routes/projectRoutes'
+import { corsConfig } from './config/cors';
+import cors from 'cors'
 
 dotenv.config() // cargar variables definidas en fichero .env
 
@@ -9,6 +11,8 @@ connectDB()
 
 
 const server = express()
+
+server.use(cors(corsConfig))
 
 // habilitar json
 server.use(express.json())
